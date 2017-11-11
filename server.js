ddec2fc4d934c44be4d65cc78b68bf2be3b18951
Dustin <dyelland@gmail.com>
2017-11-11 17:06:06 -0600

    playing with more grids

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const Router = require('express').Router;
+const express = require('express');
+require('dotenv').config();
 
-const router = new Router();
+const app = express();
+const PORT = process.env.PORT;
 
-router.get('/',(req,res,next) => {
+app.use(express.static('./public'));
 
-})
+app.get('*',(req,res,next) => {
+  res.sendFile('index.html',{root: './public'});
+});
+
+app.listen(PORT,() => {
+  console.log(`Listening on port ${PORT}...`);
+});
